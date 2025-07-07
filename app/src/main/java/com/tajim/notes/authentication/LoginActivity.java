57cc0b51d7ed7afa3a8344f03c868218dcb5f324
TR Tajim <com.tajim@gmail.com>
2025-07-07 12:52:04 +0600

    feat: create Signup, Signin, and Splash activities

---
+++ app/src/main/java/com/tajim/notes/authentication/LoginActivity.java
@@ -0,0 +1,21 @@
+package com.tajim.notes.authentication;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.tajim.notes.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+    }
+}
