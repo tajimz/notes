04ebd7d829a4fee797933b039f8737546ae6615e
TR Tajim <com.tajim@gmail.com>
2025-07-07 13:12:12 +0600

    feat: design splash activty and class basic

--- app/src/main/java/com/tajim/notes/others/SplashActivity.java
+++ app/src/main/java/com/tajim/notes/others/SplashActivity.java
@@ -1,6 +1,8 @@
 package com.tajim.notes.others;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,14 +10,25 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.tajim.notes.MainActivity;
 import com.tajim.notes.R;
+import com.tajim.notes.utils.BaseActivity;
 
-public class SplashActivity extends AppCompatActivity {
+public class SplashActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        endSplash(4000);
 
+
+    }
+
+    private void endSplash(int delayTimeInMS){
+        delayTime(delayTimeInMS, ()->{
+            startActivity(new Intent(SplashActivity.this, MainActivity.class));
+            finish();
+        });
     }
 }
--- app/src/main/java/com/tajim/notes/utils/BaseActivity.java
+++ app/src/main/java/com/tajim/notes/utils/BaseActivity.java
@@ -1,4 +1,19 @@
 package com.tajim.notes.utils;
 
-public class BaseActivity {
+import android.os.Handler;
+import androidx.appcompat.app.AppCompatActivity;
+
+public abstract class BaseActivity extends AppCompatActivity {
+
+
+    protected void delayTime (int timeInMS, Runnable toRun){
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                toRun.run();
+            }
+        }, timeInMS);
+    }
+
+
 }
