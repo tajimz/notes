1a6c83a7aa15be37db84efcd02755fe41c0c5fbf
TR Tajim <com.tajim@gmail.com>
2025-07-08 22:01:30 +0600

    feat: implement fully functional Signup feature

--- app/src/main/java/com/tajim/notes/authentication/LoginActivity.java
+++ app/src/main/java/com/tajim/notes/authentication/LoginActivity.java
@@ -1,12 +1,16 @@
 package com.tajim.notes.authentication;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.widget.Toast;
+
+import com.tajim.notes.MainActivity;
 import com.tajim.notes.databinding.ActivityLoginBinding;
 import com.tajim.notes.utils.BaseActivity;
 import com.tajim.notes.utils.CONSTANTS;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 public class LoginActivity extends BaseActivity {
@@ -23,6 +27,7 @@ public class LoginActivity extends BaseActivity {
         binding.tvSignup.setOnClickListener(v->{startActivity(new Intent(LoginActivity.this, SignupActivity.class));});
 
         handleLogin();
+        handleForgotPass();
 
 
 
@@ -44,6 +49,18 @@ public class LoginActivity extends BaseActivity {
                 @Override
                 public void onSuccess(JSONObject result) {
 
+                    try {
+                        String status = result.getString("status");
+                        if (status.equals("success")) loginSuccess(email, password);
+                        else alert("Alert", status,  ()->{});
+                    } catch (JSONException e) {
+                        throw new RuntimeException(e);
+                    }
+
+
+
+
+
                 }
             });
 
@@ -53,9 +70,24 @@ public class LoginActivity extends BaseActivity {
 
     private boolean isValidInput(String email, String password){
         if (email.isEmpty() || password.isEmpty()){
-            alert("Input Required", "Please fill in all the required fields.");
+            alert("Input Required", "Please fill in all the required fields.", ()->{});
             return false;
         }
         return true;
+    }
+
+    protected void handleForgotPass(){
+        binding.tvForgot.setOnClickListener(v->{
+            alert("Forgot Password?",
+                    "As you know, the password cannot be reset directly. Please visit the website below and submit your email. If your email is correct, you will receive your password hint within 24 hours.", () -> {startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://trtajim.xyz/apps/notes/authentication/forgot.html")));});
+        });
+    }
+    private void loginSuccess(String email, String password){
+        editSharedPref(CONSTANTS.EMAIL, email);
+        editSharedPref(CONSTANTS.PASSWORD, password);
+
+        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
     }
 }
