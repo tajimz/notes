d1e3ad2ecc3d32846501568ab1e7039e48779751
TR Tajim <com.tajim@gmail.com>
2025-07-09 23:53:20 +0600

    feat: show notes in mainActivity via recyclerView

--- app/src/main/java/com/tajim/notes/MainActivity.java
+++ app/src/main/java/com/tajim/notes/MainActivity.java
@@ -7,7 +7,9 @@ import android.util.Log;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
+import com.tajim.notes.adapters.RecyclerAdapterMain;
 import com.tajim.notes.databinding.ActivityMainBinding;
 import com.tajim.notes.notes.AddNoteActivity;
 import com.tajim.notes.others.SqliteHelper;
@@ -15,43 +17,29 @@ import com.tajim.notes.others.SqliteHelper;
 public class MainActivity extends AppCompatActivity {
     ActivityMainBinding binding;
     SqliteHelper sqliteHelper;
-
+    RecyclerAdapterMain recyclerAdapterMain;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         sqliteHelper = new SqliteHelper(this);
+        recyclerAdapterMain = new RecyclerAdapterMain(sqliteHelper);
 
         binding.addNotes.setOnClickListener(v->{startActivity(new Intent(MainActivity.this, AddNoteActivity.class));});
 
+        binding.recyclerMain.setAdapter(recyclerAdapterMain);
+        binding.recyclerMain.setLayoutManager(new LinearLayoutManager(this));
 
     }
 
-    private void loadData(){
-        Cursor cursor = sqliteHelper.getData();
-        try{
-            while (cursor.moveToNext()){
-                String title, body, date, id;
-                title = cursor.getString(1);
-                body = cursor.getString(2);
-                date = cursor.getString(3);
-                id = cursor.getString(4);
-                Log.d("allData", "\n\ntitle: "+ title+"\nbody: " + body + "\ndate: "+date + "\nid: "+id);
 
-            }
-        }finally {
-            cursor.close();
-        }
-
-
-    }
 
 
     @Override
     protected void onPostResume() {
         super.onPostResume();
-        loadData();
-        Toast.makeText(this, "ersume called", Toast.LENGTH_SHORT).show();
+        recyclerAdapterMain.refreshData();
+
     }
 }
---
+++ app/src/main/java/com/tajim/notes/adapters/RecyclerAdapterMain.java
@@ -0,0 +1,66 @@
+package com.tajim.notes.adapters;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.tajim.notes.databinding.LayoutRecyclerMainBinding;
+import com.tajim.notes.others.SqliteHelper;
+
+public class RecyclerAdapterMain extends RecyclerView.Adapter<RecyclerAdapterMain.ViewHolderMain> {
+    SqliteHelper sqliteHelper;
+    Cursor cursor;
+    public RecyclerAdapterMain(SqliteHelper sqliteHelper){
+        this.sqliteHelper = sqliteHelper;
+        cursor = sqliteHelper.getData();
+    }
+    public class ViewHolderMain extends RecyclerView.ViewHolder{
+        LayoutRecyclerMainBinding binding;
+        public ViewHolderMain(LayoutRecyclerMainBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
+        }
+    }
+    @NonNull
+    @Override
+    public ViewHolderMain onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        LayoutRecyclerMainBinding binding = LayoutRecyclerMainBinding.inflate(inflater, parent, false);
+
+        return new ViewHolderMain(binding);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolderMain holder, int position) {
+        cursor.moveToPosition(position);
+        String title = cursor.getString(1);
+        String body = cursor.getString(2);
+        String date = cursor.getString(3);
+        String id = cursor.getString(4);
+
+        holder.binding.tvTitle.setText(title);
+        holder.binding.tvBody.setText(date + " " + body);
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return cursor.getCount();
+    }
+
+    public void refreshData() {
+        if (cursor != null && !cursor.isClosed()) {
+            cursor.close();
+        }
+        cursor = sqliteHelper.getData();
+        notifyDataSetChanged();
+    }
+
+
+}
