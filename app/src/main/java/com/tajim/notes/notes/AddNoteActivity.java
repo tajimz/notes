75ff7828132df70f91849d5c1fce827b461baceb
tajim <com.tajim@gmail.com>
2025-07-11 16:25:36 +0600

    feat: add options to export notes

--- app/src/main/java/com/tajim/notes/notes/AddNoteActivity.java
+++ app/src/main/java/com/tajim/notes/notes/AddNoteActivity.java
@@ -190,6 +190,7 @@ public class AddNoteActivity extends BaseActivity {
 
     }
 
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
